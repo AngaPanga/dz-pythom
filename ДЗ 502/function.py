d0e45aa5ae2435edb5faf3cbf7a8887b2905c8ed
AngaPanga <w13w13@yandex.ru>
2022-09-23 19:34:52 +0300

    DZ pythom 01-03, rework

--- ДЗ 502/function.py
+++ ДЗ 502/function.py
@@ -7,5 +7,8 @@ n = int(input('Введите число N: '))
 result_array = []
 for i in range (1, n+1):
     result_array.append(function_calculator(i))
-print(*result_array)
+sum = 0
+for j in result_array:
+    sum += j
+print(f'Сумма {n} элементов функции = ', sum)
 input()
